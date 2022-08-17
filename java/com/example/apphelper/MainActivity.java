917d7f479b04b950f9d9d02959fdc7cf05c82563
diananeagoe <87384095+diananeagoe@users.noreply.github.com>
2022-08-17 14:22:10 +0300

    Add files via upload

--- java/com/example/apphelper/MainActivity.java
+++ java/com/example/apphelper/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.apphelper;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -29,11 +30,15 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_dashboard)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    public Context getContext(){
+        return this;
     }
 
 }
